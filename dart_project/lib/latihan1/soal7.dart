34ee9a36b89a424e604ba4777b77bb53f3ff6dd2
yzr1e <mohfahrih24@gmail.com>
2025-05-01 19:03:39 +0700

    Soal 7

---
+++ dart_project/lib/latihan1/soal7.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Masukkan bilangan pertama: ');
+  int bilangan1 = int.parse(stdin.readLineSync()!);
+  
+  stdout.write('Masukkan bilangan kedua: ');
+  int bilangan2 = int.parse(stdin.readLineSync()!);
+
+  int hasilBagi = bilangan1 ~/ bilangan2;
+  int sisa = bilangan1 % bilangan2;
+
+  print('Hasil bagi: $hasilBagi, Sisa: $sisa');
+}
